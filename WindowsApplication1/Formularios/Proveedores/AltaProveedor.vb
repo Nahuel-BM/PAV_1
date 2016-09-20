d279c6e6199e552416ce5a2e51c503eeb7432b06
fran213 <frugiero@hotmail.com>
2016-09-20 00:07:54 -0300

    proveedores !
    alta y baja proveedroes

---
+++ WindowsApplication1/Formularios/Proveedores/AltaProveedor.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+
+    Dim Conexion As Conexion = Constantes.accesoMySQL
+    Dim Funciones As New FuncionesUtiles
+
+    Private Sub AltaPersona_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Conexion.cargarComboTipo(Me.cmb_provincia, "Provincia")
+        Conexion.cargarComboTipo(Me.cmb_localidad, "Localidad", " WHERE `Provincia` = 1 ORDER BY `Nombre` ASC ;")
+    End Sub
+
+    Private Sub btn_aceptar_Click(sender As Object, e As EventArgs) Handles btn_aceptar.Click
+        Dim guardar As Boolean = True
+        If Funciones.ValidarTextBox(Me.txt_nombre) = False Then
+            guardar = False
+            MsgBox("Revise el campo Nombre")
+        End If
+
+        
+        If Funciones.ValidarTextBox(Me.txt_calle) = False Then
+            guardar = False
+            MsgBox("Revise el campo Calle")
+        End If
+
+        If Funciones.ValidarTextBox(Me.txt_numero) = False Then
+            guardar = False
+            MsgBox("Revise el campo Número de calle")
+        End If
+
+        If guardar = True Then
+
+            Dim calle As String = Funciones.QuitarEspacios(Me.txt_calle.Text)
+            Dim numero As Integer = Integer.Parse(Funciones.QuitarTodosLosEspacios(Me.txt_numero.Text))
+            Dim localidad As Integer = Me.cmb_localidad.SelectedValue
+
+
+            Dim idDomicilio As Integer = Conexion.CrearDomicilio(calle, numero, localidad)
+
+            Dim nombre As String = Funciones.QuitarEspacios(Me.txt_nombre.Text)
+            Try
+                Conexion.CrearProveedor(idDomicilio, nombre)
+                MsgBox("Proveedor creado correctamente")
+            Catch exception As Exception
+                MsgBox("Error en la Creacion del proveedor. " & exception.Message)
+                Conexion.BorrarDomicilio(idDomicilio)
+            End Try
+        End If
+    End Sub
+
+    Private Sub btn_cancelar_Click(sender As Object, e As EventArgs) Handles btn_cancelar.Click
+        Me.Dispose()
+    End Sub
+
+    Private Sub cmb_provincia_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmb_provincia.SelectedIndexChanged
+
+    End Sub
+End Class
---
+++ WindowsApplication1/Formularios/Proveedores/BajaProveedor.vb
@@ -0,0 +1,38 @@
+﻿Public Class BajaProveedor
+
+    Dim Conexion As Conexion = Constantes.accesoMySQL
+    Dim Funciones As New FuncionesUtiles
+    Dim editar As Boolean = False
+
+    Private Sub BajaPersona_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If editar = True Then
+            Funciones.AddButtonColumn(Me.DataGridView1, "Editar", "Acción", 3)
+        Else
+            Funciones.AddButtonColumn(Me.DataGridView1, "Eliminar", "Acción", 3)
+        End If
+
+    End Sub
+
+    Private Sub btn_buscar_Click(sender As Object, e As EventArgs) Handles btn_buscar.Click
+
+        Me.DataGridView1.Rows.Clear()
+
+        Dim numero As String = Funciones.QuitarTodosLosEspacios(Me.txt_nroproveedor.Text)
+        Dim tabla As DataTable = Conexion.Consulta("SELECT `Domicilio`.`Nombre`, `Proveedor`.`id`, `Proveedor`.`Nombre` FROM `Proveedor` JOIN `Domicilio` ON `Proveedor`.`Domicilio` = `Domicilio`.`id` WHERE `Proveedor`.`id` = " & numero & ";")
+
+        Dim recorrido As Integer = 0
+        For recorrido = 0 To tabla.Rows.Count() - 1
+            Me.DataGridView1.Rows.Add()
+
+            Me.DataGridView1.Rows(recorrido).Cells("id").Value = tabla.Rows(recorrido)("id")
+            Me.DataGridView1.Rows(recorrido).Cells("nombre").Value = tabla.Rows(recorrido)("Nombre")
+            Me.DataGridView1.Rows(recorrido).Cells("domicilio").Value = tabla.Rows(recorrido)("Domicilio")
+
+        Next
+
+    End Sub
+
+    Private Sub btn_salir_Click(sender As Object, e As EventArgs) Handles btn_salir.Click
+        Me.Dispose()
+    End Sub
+End Class
